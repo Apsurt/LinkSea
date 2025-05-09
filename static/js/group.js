f46ea3f19032ff2371290ffb81039977bfa74aea
Apsurt <tymon@becella.eu>
2025-05-09 19:43:15 +0200

    Split group.html

---
+++ static/js/group.js
@@ -0,0 +1,97 @@
+function toggleRenameForm() {
+  const renameForm = document.getElementById("rename-group-form");
+  renameForm.style.display =
+    renameForm.style.display === "none" ? "flex" : "none";
+}
+
+function copyGroupId(groupId) {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard
+      .writeText(groupId)
+      .then(() => {
+        showToast("Group ID copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy using clipboard API: ", err);
+        fallbackCopyTextToClipboard(groupId);
+      });
+  } else {
+    fallbackCopyTextToClipboard(groupId);
+  }
+}
+
+function fallbackCopyTextToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    const successful = document.execCommand("copy");
+    const msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+    showToast("Group ID copied to clipboard!");
+  } catch (err) {
+    console.error("Fallback: Unable to copy", err);
+    alert("Unable to copy Group ID to clipboard.");
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function showToast(message) {
+  const toast = document.getElementById("copy-toast");
+  toast.textContent = message;
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, 2000); // Hide toast after 2 seconds
+}
+
+const linkList = document.querySelector(".link-list");
+let draggedItem = null;
+
+linkList.addEventListener("dragstart", (e) => {
+  draggedItem = e.target;
+  e.target.classList.add("dragging");
+});
+linkList.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+  draggedItem = null;
+});
+linkList.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  const afterElement = getDragAfterElement(linkList, e.clientY);
+  if (afterElement == null) {
+    linkList.appendChild(draggedItem);
+  } else {
+    linkList.insertBefore(draggedItem, afterElement);
+  }
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [
+    ...container.querySelectorAll(".link-item:not(.dragging)"),
+  ];
+  return draggableElements.reduce(
+    (closest, child) => {
+      const box = child.getBoundingClientRect();
+      const offset = y - box.top - box.height / 2;
+
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child };
+      } else {
+        return closest;
+      }
+      return closest;
+    },
+    { offset: Number.NEGATIVE_INFINITY },
+  ).element;
+}
